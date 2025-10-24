cb3846472fbcdf1873bb767264ddeadedb76aef6
Gustavo Freitas <100533677+gustavfreitas@users.noreply.github.com>
2025-10-24 18:51:19 -0300

    Add files via upload

---
+++ SQL_QUERY_24_10_2025_PT2.sql
@@ -0,0 +1,26 @@
+-- 8 - Quantos funcionários estão em cada profissão? Agrupe por JobId.
+SELECT COUNT(*) AS DISTRUIÇÃO_DE_FUNCIONÁRIOS_POR_SETOR
+FROM Employee
+group by job_id
+
+-- 9 - Qual o preço do livro mais barato de cada tipo?
+SELECT MIN(PRICE) AS MENOR_PREÇO, type
+FROM TITLES
+GROUP BY type
+
+-- 10 - Quantos autores vivem em cada estado?
+SELECT COUNT(*) AS AUTORES_RESIDENTES, state
+FROM authors
+GROUP BY state
+
+-- 11 - Quantos livros foram publicados em cada ano?
+SELECT COUNT(*) AS LIVROS_PUBLICADOS, pubdate
+FROM titles
+GROUP BY pubdate
+ORDER BY pubdate ASC
+
+-- 12 (Hard) - Crie uma relação que mostra quantos livros começam com cada letra.
+SELECT UPPER(SUBSTRING(title, 1,1)) AS LETRA_INICIAL, COUNT(*) AS TOTAL
+FROM titles
+GROUP BY title
+ORDER BY title
