95af479fcaf9a296b70ccaaee7cdde0bc13c98c7
Gustavo Freitas <100533677+gustavfreitas@users.noreply.github.com>
2025-10-31 19:57:01 -0300

    Refactor SQL queries and add missing conditions

--- SQL_QUERY_31_10_2025_PT2.sql
+++ SQL_QUERY_31_10_2025_PT2.sql
@@ -18,21 +18,23 @@ FROM TIPOPOKEMON
 SELECT PRIMARIO.nome, EVOLUCAO.evoluiDe
 FROM Pokemon AS PRIMARIO INNER JOIN POKEMON AS EVOLUCAO ON PRIMARIO.codPokemon = EVOLUCAO.evoluiDe
 
--- 8 - Exiba uma relação de cada pokémon e seus tipos.
+-- 8 - Exiba uma relaÃ§Ã£o de cada pokÃ©mon e seus tipos.
 
 SELECT P.nome, T.nmTipo
 FROM Pokemon as P Inner Join TipoPokemon as TP ON P.codPokemon = TP.codPokemon INNER JOIN Tipo AS T ON TP.codTipo = T.codTipo
 ORDER BY P.codPokemon ASC
 
--- 9 - Exiba uma relação de todos os pokemon que tem fraqueza para "steel".
+-- 9 - Exiba uma relaÃ§Ã£o de todos os pokemon que tem fraqueza para "steel".
 
 SELECT P.nome, T.nmTipo
 FROM Pokemon as P Inner Join Fraqueza as F ON P.codPokemon = F.codPokemon INNER JOIN Tipo AS T ON F.codTipo = T.codTipo
+WHERE T.nmTipo = 'steel'
 ORDER BY P.codPokemon ASC
 
--- 10 - Quantos pokemons existem em cada categoria? (exiba uma relação com o nome da categoria e a quantidade de pokemons)
+-- 10 - Quantos pokemons existem em cada categoria? (exiba uma relaÃ§Ã£o com o nome da categoria e a quantidade de pokemons)
 
 SELECT C.nmCategoria, COUNT(P.codPokemon) as QUANTIDADE_POKEMONS
 FROM Pokemon AS P INNER JOIN Categoria AS C ON P.codCategoria = C.codCategoria
 GROUP BY C.nmCategoria
 ORDER BY COUNT(P.codPokemon) DESC
+
